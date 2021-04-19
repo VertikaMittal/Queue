a1258a838d06be95ec880998e7d7dc702973f4bb
Vertika <mittalvertika1803200@gmail.com>
2021-04-20 04:25:23 +0530

    first commit

---
+++ usingArray.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct queue{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+void create(struct queue*q,int size)
+{
+    q->size=size;
+    q->front=q->rear=-1;
+    q->Q=(int*)malloc(q->size*sizeof(int));
+}
+void enqueue(struct queue*q,int x)
+{
+    if(q->rear==q->size-1)
+    {   
+        printf("queue is full");
+    }
+    else
+    {
+        q->rear++;
+        q->Q[q->rear]=x;
+    }
+}
+int dequeue(struct queue*q)
+{
+    int x=-1;
+    if(q->rear==q->front)
+    {   
+        printf("queue is empty");
+    }
+    else
+    {
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+    
+}
+void display(struct queue q)
+{
+    int x;
+    for(int i=q.front+1;i<=q.rear;i++)
+    {
+        printf("%d ",q.Q[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    struct queue q;
+    create(&q,5);
+    enqueue(&q,10);
+    enqueue(&q,20);
+    enqueue(&q,30);
+    // enqueue(&q,40);
+    // enqueue(&q,50);
+    printf("%d ",dequeue(&q));
+    printf("\n");
+    display(q);
+
+}
